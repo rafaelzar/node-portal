7484741b5d69769a729372056cce709b08165aad
rafaelzar <rafaelzaerate@gmail.com>
2021-08-05 14:52:27 +0200

    Create Employee Schema for Employee model

--- src/employees/models/EmployeesModel.ts
+++ src/employees/models/EmployeesModel.ts
@@ -1,6 +1,36 @@
-import { Types, Schema, model } from "mongoose";
-import { Employee } from "eyerate";
-export const EmployeeSchema: Schema = new Schema({}, { collection: "employees" });
+import { Types, Schema, model } from 'mongoose';
+import { Employee } from 'eyerate';
 
-const EmployeeModel = model<Employee>("Users", EmployeeSchema);
+export const EmployeeSchema: Schema = new Schema(
+  {
+    location: {
+      type: Types.ObjectId,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+    },
+    first_name: {
+      type: String,
+      require: true,
+    },
+    last_name: {
+      type: String,
+      require: true,
+    },
+    nick_names: {
+      type: Array,
+    },
+    active: {
+      type: Boolean,
+    },
+  },
+  { collection: 'employees', timestamps: true },
+);
+
+const EmployeeModel = model<Employee>('Users', EmployeeSchema);
 export default EmployeeModel;
