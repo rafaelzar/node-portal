55b4f10638c457edab1e0c896b62a7627a46017e
rafaelzar <rafaelzaerate@gmail.com>
2021-08-05 11:38:19 +0200

    Finish ErrorHandler class

--- src/errors/ErrorHandler.ts
+++ src/errors/ErrorHandler.ts
@@ -1,5 +1,7 @@
 export default class ErrorHandler extends Error {
   constructor(public statusCode: number, public message: string) {
     super();
+    this.statusCode = statusCode
+    this.message = message
   }
 }
