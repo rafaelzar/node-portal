049027f1f407606cf0db003582f6d32cf9ca561f
rafaelzar <rafaelzaerate@gmail.com>
2021-08-16 11:10:45 +0200

    Add location and review model

--- src/employees/EmployeesRouter.ts
+++ src/employees/EmployeesRouter.ts
@@ -19,7 +19,7 @@ class EmployeesRouter {
   }
 
   private _configure() {
-    this._router.get('/validate-jwt', this._controller.validateJwt);
+    this._router.get('/validate-jwt', validateJWT, this._controller.validateJwt);
     this._router.patch('/:id', validateMongoId, updateEmployeeDto, validateJWT, this._controller.updateEmployee);
   }
 }
---
+++ src/employees/models/ReviewsModel.ts
@@ -0,0 +1,6 @@
+import { Schema, model } from 'mongoose';
+import { Review } from 'eyerate';
+export const ReviewSchema: Schema = new Schema({}, { collection: 'Reviews' });
+
+const ReviewModel = model<Review>('Reviews', ReviewSchema);
+export default ReviewModel;
