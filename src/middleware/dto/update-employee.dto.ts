f7ae3ce8a18f25ac7f193a0d6b028dd0506b9e2e
rafaelzar <rafaelzaerate@gmail.com>
2021-08-18 13:47:19 +0200

    Add nick_name validatior to dto

--- src/middleware/dto/update-employee.dto.ts
+++ src/middleware/dto/update-employee.dto.ts
@@ -6,7 +6,8 @@ export const updateEmployeeDto = (req: Request, res: Response, next: NextFunctio
     if (
       !req.body.hasOwnProperty('first_name') ||
       !req.body.hasOwnProperty('last_name') ||
-      !req.body.hasOwnProperty('phone')
+      !req.body.hasOwnProperty('phone') ||
+      !req.body.hasOwnProperty('nick_names')
     ) {
       throw new ErrorHandler(400, 'Invalid data transfer object');
     }
@@ -19,7 +20,9 @@ export const updateEmployeeDto = (req: Request, res: Response, next: NextFunctio
     if (typeof req.body.phone !== 'string') {
       throw new ErrorHandler(400, 'property property must be string type');
     }
-
+    if (typeof req.body.nick_names !== 'object') {
+      throw new ErrorHandler(400, 'nick_names property must be object type');
+    }
     if (req.body.first_name.length > 100 || req.body.last_name.length < 3) {
       throw new ErrorHandler(400, 'first_name property maximum length is 100 and minimum 3');
     }
@@ -28,6 +31,9 @@ export const updateEmployeeDto = (req: Request, res: Response, next: NextFunctio
     }
     if (req.body.phone.length > 50 || req.body.phone.length < 5) {
       throw new ErrorHandler(400, 'phone property maximum lenght is 50 and minimum 5');
+    }
+    if (!req.body.nick_names.length) {
+      throw new ErrorHandler(400, 'nick_names property must have at least one nick name');
     }
 
     next();
