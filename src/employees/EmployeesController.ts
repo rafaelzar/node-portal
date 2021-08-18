ce8bacf2307e1041d43d760004f8c5b987f825f0
rafaelzar <rafaelzaerate@gmail.com>
2021-08-18 16:54:11 +0200

    Refactor average rate method

--- src/employees/EmployeesController.ts
+++ src/employees/EmployeesController.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import { Employee, Review, Mention } from 'eyerate';
+import { Employee, Location, Review, Mention, Conversation } from 'eyerate';
 import EmployeeModel from './models/EmployeesModel';
 import { Model, Types } from 'mongoose';
 import ErrorHandler from '../errors/ErrorHandler';
-import MentionModel from './models/MentionModel';
+import LocationModel from './models/MentionModel';
 import ReviewModel from './models/ReviewsModel';
+import MentionModel from './models/MentionModel';
 
 class EmployeesController {
   constructor(
@@ -17,7 +18,6 @@ class EmployeesController {
     try {
       if (!req.user) throw new ErrorHandler(404, 'Cognito user not found');
       const user = await this.employeeModel.findOne({ email: req.user.email });
-
       if (!user) throw new ErrorHandler(404, 'Employee not found');
       res.send(user);
     } catch (error) {
@@ -67,36 +67,6 @@ class EmployeesController {
       }
       const averageRating = sumReview / reviews.length;
       res.send({ averageRating });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async getReviews(req: Request, res: Response, next: NextFunction) {
-    try {
-      const queryObj = req.queryObj as any;
-      const sort = req.query.sort as string;
-      const eyerate = req.query.eyerate;
-      const sortBy = req.query.sortBy as string;
-      const page = parseInt(req.query.page as string);
-      const size = 5;
-
-      const userId = req.params.id;
-      const mentions = await this.mentionModel.find({ employee: Types.ObjectId(userId) }, { review: 1, _id: 0 });
-      const reviewIds = mentions.map((el) => el.toObject().review);
-
-      queryObj.$and.push({ _id: { $in: reviewIds } });
-
-      const reviewCount = await this.reviewModel.find(queryObj).countDocuments();
-      const reviews = await this.reviewModel
-        .find(queryObj)
-        .sort({ [sortBy]: sort })
-        .select('date content rating platform author')
-        .skip((page - 1) * size)
-        .limit(size);
-      console.log(reviews);
-
-      res.send({ data: reviews, pageCount: Math.floor(reviewCount / size) + 1 });
     } catch (error) {
       next(error);
     }
