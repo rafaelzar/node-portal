d5a07d684189b4f8186d4f040cad0f7068971481
rafaelzar <rafaelzaerate@gmail.com>
2021-08-23 16:42:53 +0200

    Add aggregation and pagination v1

--- src/middleware/validate-query-param.ts
+++ src/middleware/validate-query-param.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Response, Request } from 'express';
+import { isValidObjectId, Types } from 'mongoose';
 import ErrorHandler from '../errors/ErrorHandler';
 
 export const validateQueryParam = (req: Request, _: Response, next: NextFunction) => {
@@ -70,23 +71,61 @@ export const validateQueryParam = (req: Request, _: Response, next: NextFunction
         throw new ErrorHandler(400, 'Eyerate must be either true or false');
       }
     }
-    const { sort, sortBy } = req.query;
-    if (sort && sortBy) {
-      if (!(sort === 'desc' || sort === 'asc')) {
-        throw new ErrorHandler(400, 'Sort must be either desc or asc');
-      }
-      if (!(sortBy === 'date' || sortBy === 'rating')) {
-        throw new ErrorHandler(400, 'SortBy must be either date or rating');
-      }
-    } else if ((!sort && sortBy) || (sort && !sortBy)) {
-      throw new ErrorHandler(400, 'Both sort and sortBy params required');
+    const { sort } = req.query;
+
+    if (sort && !(sort === 'desc' || sort === 'asc')) {
+      throw new ErrorHandler(400, 'Sort must be either desc or asc');
     }
+
     if (req.query.page) {
       const page = Number(req.query.page);
       if (isNaN(page) || page < 1) {
         throw new ErrorHandler(400, 'Page must be a number greater than equal to 1');
       }
     }
+
+    if (req.query.firstDate && req.query.lastDate)
+      throw new ErrorHandler(400, 'Only one firstDate/lastDate param can be passed');
+    if (req.query.rid && !(req.query.lastDate || req.query.firstDate))
+      throw new ErrorHandler(400, 'Eid should be provided with lastDate or firstDate param');
+    else {
+      if (!isValidObjectId(req.query.rid as string)) throw new ErrorHandler(400, 'Not a valid mongoid');
+      if (req.query.lastDate) queryObj.$and.push({ _id: { $ne: Types.ObjectId(req.query.rid as string) } });
+      if (req.query.firstDate) queryObj.$and.push({ _id: { $ne: Types.ObjectId(req.query.rid as string) } });
+    }
+    if (req.query.lastDate && !(req.query.cursor == 'left' || req.query.cursor == 'right'))
+      throw new ErrorHandler(400, 'Cursors must be either left or right');
+    const { cursor } = req.query;
+    if (req.query.lastDate && cursor === 'right') {
+      const lastDate = new Date(req.query.lastDate as string);
+      queryObj.$and = queryObj.$and.map((obj: any) => {
+        if (Object.keys(obj).includes('date')) {
+          obj.date.$gt = lastDate;
+        }
+        if (Object.keys(obj).includes('created_at')) {
+          obj['created_at'].$gt = lastDate;
+        }
+
+        return obj;
+      });
+    }
+
+    if (req.query.lastDate && cursor === 'left') {
+      const lastDate = new Date(req.query.lastDate as string);
+      queryObj.$and = queryObj.$and.map((obj: any) => {
+        if (Object.keys(obj).includes('date')) {
+          obj.date.$lt = lastDate;
+        }
+        if (Object.keys(obj).includes('created_at')) {
+          obj['created_at'].$lt = lastDate;
+        }
+
+        return obj;
+      });
+    }
+    if (req.query.firstDate) {
+    }
+    console.log(queryObj.$and.map((el: any) => el));
     req.queryObj = queryObj;
     next();
   } catch (error) {
