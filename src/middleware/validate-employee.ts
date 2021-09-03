a05a0cbd125e66d61608c47ef0c0c5ad21ab9b4d
rafaelzar <rafaelzaerate@gmail.com>
2021-09-03 13:17:36 +0200

    Add validate employee middleware

--- src/middleware/validate-employee.ts
+++ src/middleware/validate-employee.ts
@@ -7,7 +7,7 @@ export const validateEmployee: RequestHandler = async (req, _res, next) => {
     const user = req.user || {};
     const employee = await EmployeeModel.findOne({ _id: req.params.id });
     if (!employee) throw new ErrorHandler(404, 'Employee not found');
-    if (employee.cognito_id !== user.aud) throw new ErrorHandler(400, 'Cant access this route');
+    if (employee.cognito_id !== user.sub) throw new ErrorHandler(400, 'Cant access this route');
     next();
   } catch (error) {
     next(error);
