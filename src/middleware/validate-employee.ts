4124611b633d042cb4d32ce173a551a7e189c8cc
rafaelzar <rafaelzaerate@gmail.com>
2021-09-03 13:01:42 +0200

    Remove secound next function in middleware

--- src/middleware/validate-employee.ts
+++ src/middleware/validate-employee.ts
@@ -9,7 +9,6 @@ export const validateEmployee: RequestHandler = async (req, _res, next) => {
     if (!employee) throw new ErrorHandler(404, 'Employee not found');
     if (employee.cognito_id !== user.aud) throw new ErrorHandler(400, 'Cant access this route');
     next();
-    next();
   } catch (error) {
     next(error);
   }
