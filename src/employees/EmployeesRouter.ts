855eaf6c06738dbf7be6241da267a37d7fa4f0a0
rafaelzar <rafaelzaerate@gmail.com>
2021-09-09 11:54:59 +0200

    Add validate-jwt middleware to routes

--- src/employees/EmployeesRouter.ts
+++ src/employees/EmployeesRouter.ts
@@ -24,8 +24,8 @@ class EmployeesRouter {
   private _configure() {
     this._router.get('/validate-jwt', validateJWT, this._controller.getEmployee);
     this._router.patch('/:id', validateMongoId, updateEmployeeDto, validateJWT, this._controller.updateEmployee);
-    this._router.get('/reviews/:id', validateMongoId, validateQueryParam, this._controller.getReviews);
-    this._router.get('/stats/:id', validateMongoId, validateQueryParam, this._controller.userStats);
+    this._router.get('/reviews/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.getReviews);
+    this._router.get('/stats/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.userStats);
     this._router.get('/create-link-token/:id', validateMongoId, validateJWT, this._controller.createLinkToken);
     this._router.post(
       '/token-exchange/:id',
@@ -36,7 +36,7 @@ class EmployeesRouter {
     );
     this._router.get('/bank-accounts/:id', validateMongoId, validateJWT, this._controller.getBankAccounts);
     this._router.get('/revenue/:id', validateJWT, validateDate, validateMongoId, this._controller.getRevenue);
-    this._router.get('/earnings/:id', this._controller.getBalanceAndLastPayment);
+    this._router.get('/earnings/:id', validateJWT, this._controller.getBalanceAndLastPayment);
     this._router.delete('/plaid-account/:id', validateJWT, validateMongoId, this._controller.removePlaidAccount);
   }
 }
