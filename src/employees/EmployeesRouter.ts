f5ec0891b37ddc78f5c164b85f766b48d80280f5
rafaelzar <rafaelzaerate@gmail.com>
2021-09-13 11:04:29 +0200

    Prepare for prod

--- src/employees/EmployeesRouter.ts
+++ src/employees/EmployeesRouter.ts
@@ -22,45 +22,101 @@ class EmployeesRouter {
     this._configure();
   }
 
-  // development routes
+  // Normal routes
   private _configure() {
     this._router.get('/validate-jwt', validateJWT, this._controller.getEmployee);
-    this._router.patch('/:id', validateMongoId, validateJWT, updateEmployeeDto, this._controller.updateEmployee);
-    this._router.get('/reviews/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.getReviews);
-    this._router.get('/stats/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.userStats);
-    this._router.get('/create-link-token/:id', validateMongoId, validateJWT, this._controller.createLinkToken);
+    this._router.patch(
+      '/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      updateEmployeeDto,
+      this._controller.updateEmployee,
+    );
+    this._router.get(
+      '/reviews/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      validateQueryParam,
+      this._controller.getReviews,
+    );
+    this._router.get(
+      '/stats/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      validateQueryParam,
+      this._controller.userStats,
+    );
+    this._router.get(
+      '/create-link-token/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      this._controller.createLinkToken,
+    );
     this._router.post(
       '/token-exchange/:id',
       validateMongoId,
       validateJWT,
+      validateEmployee,
       tokenExchangeDto,
       this._controller.exchangeToken,
     );
-    this._router.get('/bank-accounts/:id', validateMongoId, validateJWT, this._controller.getBankAccounts);
-    this._router.get('/revenue/:id', validateMongoId, validateJWT, validateDate, this._controller.getRevenue);
-    this._router.get('/earnings/:id', validateMongoId, validateJWT, this._controller.getBalanceAndLastPayment);
-    this._router.delete('/plaid-account/:id', validateMongoId, validateJWT, this._controller.removePlaidAccount);
+    this._router.get(
+      '/bank-accounts/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      this._controller.getBankAccounts,
+    );
+    this._router.get(
+      '/revenue/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      validateDate,
+      this._controller.getRevenue,
+    );
+    this._router.get(
+      '/earnings/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      this._controller.getBalanceAndLastPayment,
+    );
+    this._router.delete(
+      '/plaid-account/:id',
+      validateMongoId,
+      validateJWT,
+      validateEmployee,
+      this._controller.removePlaidAccount,
+    );
   }
 
-  // production routes
+  // Test routes
+  // they allow any user to "impersonate" any other user, and visualise data from them
+  // This was used in development since our newly created users didn't have any review and revenue data
+  // so we had to "impersonate" other users.
+
   // private _configure() {
   //   this._router.get('/validate-jwt', validateJWT, this._controller.getEmployee);
-  //   this._router.patch('/:id', validateMongoId, validateJWT, validateEmployee, updateEmployeeDto, this._controller.updateEmployee);
-  //   this._router.get('/reviews/:id', validateMongoId, validateJWT, validateEmployee, validateQueryParam, this._controller.getReviews);
-  //   this._router.get('/stats/:id', validateMongoId, validateJWT,validateEmployee ,validateQueryParam, this._controller.userStats);
-  //   this._router.get('/create-link-token/:id', validateMongoId, validateJWT, validateEmployee,this._controller.createLinkToken);
+  //   this._router.patch('/:id', validateMongoId, validateJWT, updateEmployeeDto, this._controller.updateEmployee);
+  //   this._router.get('/reviews/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.getReviews);
+  //   this._router.get('/stats/:id', validateMongoId, validateJWT, validateQueryParam, this._controller.userStats);
+  //   this._router.get('/create-link-token/:id', validateMongoId, validateJWT, this._controller.createLinkToken);
   //   this._router.post(
   //     '/token-exchange/:id',
   //     validateMongoId,
   //     validateJWT,
-  //     validateEmployee,
   //     tokenExchangeDto,
   //     this._controller.exchangeToken,
   //   );
-  //   this._router.get('/bank-accounts/:id', validateMongoId, validateJWT, validateEmployee, this._controller.getBankAccounts);
-  //   this._router.get('/revenue/:id', validateMongoId, validateJWT,validateEmployee ,validateDate, this._controller.getRevenue);
-  //   this._router.get('/earnings/:id', validateMongoId, validateJWT,validateEmployee ,this._controller.getBalanceAndLastPayment);
-  //   this._router.delete('/plaid-account/:id', validateMongoId, validateJWT, validateEmployee,this._controller.removePlaidAccount);
+  //   this._router.get('/bank-accounts/:id', validateMongoId, validateJWT, this._controller.getBankAccounts);
+  //   this._router.get('/revenue/:id', validateMongoId, validateJWT, validateDate, this._controller.getRevenue);
+  //   this._router.get('/earnings/:id', validateMongoId, validateJWT, this._controller.getBalanceAndLastPayment);
+  //   this._router.delete('/plaid-account/:id', validateMongoId, validateJWT, this._controller.removePlaidAccount);
   // }
 }
 
