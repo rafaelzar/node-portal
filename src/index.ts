dc14a972569f5afad32faa800aabf9f41c7bd912
rafaelzar <rafaelzaerate@gmail.com>
2021-12-14 15:11:25 -0800

    Revert "fix: :fire: Send email about reviews once a day"

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import ENV from './env-config';
 import App from './App';
 import { nonEyerateTrigger } from './send-grid/non-eyerate-trigger';
 import { eyerateTrigger } from './send-grid/eyerate-trigger';
-import { reviewsTrigger } from './send-grid/reviews-trigger';
 
 const mongoString = ENV.APP_DB_URI;
 const mongoOptions: ConnectOptions = {
@@ -23,7 +22,6 @@ mongoose
   .then(() => {
     nonEyerateTrigger();
     eyerateTrigger();
-    reviewsTrigger();
     const myApp = App.getInstance();
     myApp.listen(ENV.APP_PORT);
   })
